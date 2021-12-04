2b52ebd844c3472cbaa163b5923a0c9f8406d970
Jonathan Bloedow <jhb4@cornell.edu>
2021-12-04 09:35:02 -0800

    Let's see if we can get HIV demo working.

---
+++ HIV/manifest.py
@@ -0,0 +1,8 @@
+my_ep4_assets=None
+requirements = "./requirements.txt"
+schema_file="stash/schema.json"
+eradication_path="stash/Eradication"
+assets_input_dir="Assets"
+plugins_folder="stash"
+output_dl_folder="COMPS_output_downloads"
+output_files_to_get=["output/ReportHIVByAgeAndGender.csv"]
